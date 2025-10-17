25e5cde790e806082188c0ceddf10b66a0e5ab08
Alexc22_create <20243rd035@utez.edu.mx>
2025-10-16 21:38:31 -0600

    + Md valor_random.py (Se Agrego una descripccion del codigo)

--- valor_random.py
+++ valor_random.py
@@ -1,6 +1,8 @@
+
+#Descripccion : Cada vez que se llama a la función, genera un número diferente de forma aleatoria.
+
 import random  # Importa el módulo 'random', que permite generar números aleatorios.
 
 # Función generada por Alejandro
 def numero_random():  # Define una función llamada 'numero_random' sin parámetros.
     return random.randint(1, 100)  # Retorna un número entero aleatorio entre 1 y 100 (ambos incluidos).
-
