94d16209c2028afde5077998bc19c00f7c54dd72
Alexc22_create <20243rd035@utez.edu.mx>
2025-10-20 21:59:42 -0600

    Pruevas Git GUI
    Esta es una pruba de utilizar git con una GUI

--- valor_random.py
+++ valor_random.py
@@ -6,3 +6,5 @@ import random  # Importa el módulo 'random', que permite generar números aleat
 # Función generada por Alejandro
 def numero_random():  # Define una función llamada 'numero_random' sin parámetros.
     return random.randint(1, 100)  # Retorna un número entero aleatorio entre 1 y 100 (ambos incluidos).
+
+#Prueba Herramienta de git Gui
